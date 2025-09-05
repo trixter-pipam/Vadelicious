7eae4726646c6f3b6c778dea9d3eb8aaa7c40018
irfanrinaldy <ipank1412@gmail.com>
2025-09-06 07:46:49 +0800

    Menambah fungsi pengurangan dan pembagian di file 'fungsi1.js'

--- functions/fungsi1.js
+++ functions/fungsi1.js
@@ -1,5 +1,11 @@
 const add = (a, b) => a + b;
 console.log(add(2, 3));
 
+const subtract = (a, b) => a - b;
+console.log(subtract(5, 2));
+
 const multiply = (a, b) => a * b;
 console.log(multiply(2, 3));
+
+const divide = (a, b) => a / b;
+console.log(divide(6, 2));
